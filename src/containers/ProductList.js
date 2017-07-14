e7e48e9ce5575b6c1bf48985494ecf6b63b61447
trof <reatrof@gmail.com>
2017-07-14 11:36:46 +0300

    sort products by likes

--- src/containers/ProductList.js
+++ src/containers/ProductList.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Product from '../components/Product'
-import {Item} from 'semantic-ui-react';
+import {Item, Icon, Button} from 'semantic-ui-react';
 
 const productData = [
     {
@@ -10,7 +10,7 @@ const productData = [
         image: 'image-aqua.png',
         authorImage: 'daniel.jpg',
         author: 'John Smith',
-        likes: 2
+        likes: 11
     },
     {
         id: 2,
@@ -18,28 +18,61 @@ const productData = [
         description: 'Description of the second voting Item',
         image: 'image-rose.png',
         authorImage: 'elliot.jpg',
-        author: 'Nikita Trofimov',
-        likes: 6
+        author: 'Eliot Carter',
+        likes: 3
+    },
+    {
+        id: 3,
+        title: 'Third item',
+        description: 'Description of the third voting Item',
+        image: 'image-yellow.png',
+        authorImage: 'justen.jpg',
+        author: 'Justen Rose',
+        likes: 9
     }
 ]
 
 class ProductList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            Slikes: false,
+            products: productData
+        }
+        this.sortedByLikes = this.sortedByLikes.bind(this);
+    }
+    sortedByLikes() {
+        this.setState({ 
+            Slikes: !this.state.Slikes,
+            products: this.state.products.sort((a,b) => (
+                this.state.Slikes ? b.likes - a.likes : a.likes - b.likes
+            )) 
+        })
+    }
     render() {
         return (
-            <Item.Group divided>
-                {
-                    productData.map((data) => {
-                        return <Product 
-                                    key={data.id}
-                                    title={data.title} 
-                                    description={data.description}
-                                    image={data.image}
-                                    authorImage={data.authorImage}
-                                    author={data.author}
-                                    likes={data.likes}/>
-                    })
-                }
-            </Item.Group>
+            <div>
+                <div className="sortes">
+                    <span>Sorted by:</span> <br/>
+                    <Button icon onClick={this.sortedByLikes}>
+                        Likes <Icon name={this.state.Slikes ? 'caret down' : 'caret up'} />
+                    </Button>
+                </div>
+                <Item.Group divided>
+                    {
+                        this.state.products.map((data) => {
+                            return <Product 
+                                        key={data.id}
+                                        title={data.title} 
+                                        description={data.description}
+                                        image={data.image}
+                                        authorImage={data.authorImage}
+                                        author={data.author}
+                                        likes={data.likes}/>
+                        })
+                    }
+                </Item.Group>
+            </div>
         )
     }
 }
